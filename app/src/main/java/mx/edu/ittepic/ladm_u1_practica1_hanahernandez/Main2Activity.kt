b6add6f358fa7aae8327e86cdb1ec7142bf9c5ef
HanaHernandez <55150439+HanaHernandez@users.noreply.github.com>
2020-02-07 23:39:54 -0700

    Practica numero 1

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_hanahernandez/Main2Activity.kt
@@ -0,0 +1,15 @@
+package mx.edu.ittepic.ladm_u1_practica1_hanahernandez
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+    }
+}
---
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_hanahernandez/MainActivity.kt
@@ -0,0 +1,16 @@
+package mx.edu.ittepic.ladm_u1_practica1_hanahernandez
+
+import android.content.Intent
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
